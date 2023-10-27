5f26b05c4986ec1cc7fad34b23115424ced7d5c6
vananh0909 <anhb2014814@student.ctu.edu.vn>
2023-10-28 02:37:00 +0700

    Hien thi danh sach cac contact lay ve tu server

--- Contact_frontend/src/main.js
+++ Contact_frontend/src/main.js
@@ -1,6 +1,9 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import './assets/main.css'
+
+import router from "./router";
+createApp(App).use(router).mount("#app");
---
+++ Contact_frontend/src/views/ContactAdd.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="page">
+        <h4>Thêm liên hệ</h4>
+        <ContactForm :contact="contact" @submit:contact="postContact" />
+        <p>{{ message }}</p>
+    </div>
+</template>
+<script>
+import ContactForm from "../components/ContactForm.vue";
+import ContactService from "../services/contact.service";
+export default {
+    components: {
+        ContactForm,
+    },
+    data() {
+        return {
+            contact: {},
+            message: "",
+        };
+    },
+    methods: {
+        async postContact(data) {
+            try {
+                await ContactService.create(data);
+                this.message = "Thông tin được thêm thành công.";
+            } catch (error) {
+                console.log(error);
+            }
+        },
+    },
+    created() {
+        this.message = "";
+    },
+};
+</script>
